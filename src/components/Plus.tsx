028946cd69803945c066282a754b71a3ed5e59a8
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-25 23:27:28 +0300

    Update Plus.tsx

--- src/components/Plus.tsx
+++ src/components/Plus.tsx
@@ -1,28 +1,20 @@
 import { useState } from "react";
 import React from "react";
 
-type PlusPropsType = {
-  
-}
-
-
-
-
+type PlusPropsType = {};
 
 const Plus = (props: PlusPropsType) => {
   // let [inc, setInc] = useState<number>(0)
-  
 
-
-  const increase = (counter:number) => {
-    
-    return counter + 1
+  const increase = (counter: number) => {
+    return counter + 1;
   };
-  
-  
-    return (
-      <button className="control__btn" onClick={increase}>Plus</button>
-    )
-}
 
-export default Plus
+  return (
+    <button className="control__btn" onClick={increase}>
+      Plus
+    </button>
+  );
+};
+
+export default Plus;
