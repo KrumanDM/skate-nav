921d14ad8a15a78f04094e904a919f34527bce79
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-03-25 23:27:44 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,30 @@
-import React, {useState} from 'react';
-import './App.css';
-import Counter from './components/Counter';
-import Plus from './components/Plus';
-import Reset from './components/Reset';
+import React, { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+import Plus from "./components/Plus";
+import Reset from "./components/Reset";
 
-
-type CounterPropsType = number
-
+type CounterPropsType = number;
 
 export default function App() {
-    const [counter, setCounter] = useState<CounterPropsType>(0);
-    
-    const AddCount = (counter:number) => { 
+  const [counter, setCounter] = useState<CounterPropsType>(0);
 
-      setCounter()
+  const AddCount = (counter: number) => {
+    setCounter();
+  };
 
-    }
+  const ResetCount = () => {
+    setCounter(0);
+  };
 
-    const ResetCount = () => {
-  
-      setCounter(0)
-    }
-
-    
-   
-    return (
-      <div className="counter">
-        <h1>Counter</h1>
-        <Counter counter={counter}/>
-        <div className="btn__container">
-          <Plus setCounter={setCounter}/>
-          <Reset setCounter={setCounter}/>
-        </div>
+  return (
+    <div className="counter">
+      <h1>Counter</h1>
+      <Counter counter={counter} />
+      <div className="btn__container">
+        <Plus setCounter={setCounter} />
+        <Reset setCounter={setCounter} />
       </div>
-    );
-  }
+    </div>
+  );
+}
