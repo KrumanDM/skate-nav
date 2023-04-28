29c3475c4e409d88e2449931a364a5e265eebbe6
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-04-28 21:53:22 +0300

    Update Counter.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -10,7 +10,6 @@ const Counter = (props: CounterPropsType) => {
   const counterClassName = `${props.count >= 5 && "maxCount"}`
 
   return <div className={counterClassName}><b>{props.count}</b></div>;
-
 };
 
 export default Counter;
