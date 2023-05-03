943d54f9bc95dcc080778b8bcb9e2e06a33f91fc
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-05-03 22:25:19 +0300

    Update Rating.tsx

--- src/Rating.tsx
+++ src/Rating.tsx
@@ -26,6 +26,7 @@ type StarPropsType ={
    value: 1|2|3|4|5
 }
 
+
 function Star (props:StarPropsType) {
     return <span onClick={()=>{props.setValue(props.value)}}>
         {props.selected ? <b>star </b> : ' star'}
